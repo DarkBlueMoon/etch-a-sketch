483411aea157b0a44b1d12d831503a1b0e839948
Demetri Evans <darkbluemoon93@gmail.com>
2021-03-12 20:44:24 -0600

    Fixed issue with grid getting larger with more cells.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#grid-container");
+const button = document.querySelector("#reset");
 
 // Thanks to Nidhin Joseph: https://stackoverflow.com/a/57550587
 function makeRows(rows, cols) {
@@ -7,7 +8,7 @@ function makeRows(rows, cols) {
 
   for (let i = 0; i < rows * cols; i++) {
     const cell = document.createElement("div");
-    cell.textContent = i + 1;
+    // cell.textContent = i + 1;
     cell.classList.add("grid-item");
     container.appendChild(cell);
   }
@@ -20,4 +21,11 @@ cells.forEach((cell) => {
   cell.addEventListener("mouseover", () => {
     cell.classList.add("black");
   });
+});
+
+button.addEventListener("click", () => {
+  // const numCells = parseInt(prompt)
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 });
