d0c803a34647436abfc0ed48c3985953f104ea31
Demetri Evans <darkbluemoon93@gmail.com>
2021-03-12 20:14:03 -0600

    Fixed small typo with grid-template-rows. Cells now become black when moused over.

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const container = document.querySelector("#container");
 
+// Thanks to Nidhin Joseph: https://stackoverflow.com/a/57550587
 function makeRows(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
 
   for (let i = 0; i < rows * cols; i++) {
     const cell = document.createElement("div");
-    // cell.textContent = i + 1;
     cell.classList.add("grid-item");
     container.appendChild(cell);
   }
 }
 
 makeRows(16, 16);
+
+const cells = document.querySelectorAll(".grid-item");
+cells.forEach((cell) => {
+  cell.addEventListener("mouseover", () => {
+    cell.classList.add("black");
+  });
+});
