5c3ed38723094dc1b88a1207e07555d85fca7c5a
Demetri Evans <darkbluemoon93@gmail.com>
2021-03-12 20:55:12 -0600

    Can now resize the grid w/ a button click.

--- script.js
+++ script.js
@@ -2,30 +2,39 @@ const container = document.querySelector("#grid-container");
 const button = document.querySelector("#reset");
 
 // Thanks to Nidhin Joseph: https://stackoverflow.com/a/57550587
-function makeRows(rows, cols) {
+function makeGrid(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
 
   for (let i = 0; i < rows * cols; i++) {
     const cell = document.createElement("div");
-    // cell.textContent = i + 1;
     cell.classList.add("grid-item");
     container.appendChild(cell);
   }
+
+  addHoverEvents();
 }
 
-makeRows(16, 16);
+makeGrid(16, 16);
 
-const cells = document.querySelectorAll(".grid-item");
-cells.forEach((cell) => {
-  cell.addEventListener("mouseover", () => {
-    cell.classList.add("black");
+function addHoverEvents() {
+  const cells = document.querySelectorAll(".grid-item");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.classList.add("black");
+    });
   });
-});
+}
 
 button.addEventListener("click", () => {
-  // const numCells = parseInt(prompt)
+  let numCells = 0;
+  do {
+    numCells = parseInt(prompt("How many cells would you like? (Max of 100)"));
+  } while (numCells > 100 || isNaN(numCells));
+
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
+
+  makeGrid(numCells, numCells);
 });
