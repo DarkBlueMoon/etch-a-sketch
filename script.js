077dd042f86f8af14c07bf0c5748324838405447
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2023-10-19 13:47:16 -0500

    Change cell color on hover

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const GRID_ROWS = 16;
 const GRID_COLS = 16;
 const gridContainer = document.querySelector("#grid-container");
 
-
 for (let row = 0; row < GRID_ROWS; row++) {
   const gridRow = document.createElement("div");
   gridRow.classList.add("grid-row");
@@ -15,3 +14,10 @@ for (let row = 0; row < GRID_ROWS; row++) {
 
   gridContainer.appendChild(gridRow);
 }
+
+gridContainer.addEventListener("mouseover", (e) => {
+  const target = e.target;
+  if (target.classList.contains("grid-cell") && !target.classList.contains("filled")) {
+    target.classList.add("filled");
+  }
+});
