65de002194b0562debb45c5ab6f9dfb5897b5d58
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2023-10-19 13:39:42 -0500

    Create grid

---
+++ script.js
@@ -0,0 +1,17 @@
+const GRID_ROWS = 16;
+const GRID_COLS = 16;
+const gridContainer = document.querySelector("#grid-container");
+
+
+for (let row = 0; row < GRID_ROWS; row++) {
+  const gridRow = document.createElement("div");
+  gridRow.classList.add("grid-row");
+
+  for (let col = 0; col < GRID_COLS; col++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("grid-cell");
+    gridRow.appendChild(gridCell);
+  }
+
+  gridContainer.appendChild(gridRow);
+}
