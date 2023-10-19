9152bad7fb2c21c573c0df59d3695a33be5c15ee
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2023-10-19 14:04:39 -0500

    Change grid size with a button

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-const GRID_ROWS = 16;
-const GRID_COLS = 16;
+const GRID_SIZE = 16;
 const gridContainer = document.querySelector("#grid-container");
+const newGridBtn = document.querySelector("#new-grid");
 
-for (let row = 0; row < GRID_ROWS; row++) {
-  const gridRow = document.createElement("div");
-  gridRow.classList.add("grid-row");
+function setupApp() {
+  gridContainer.addEventListener("mouseover", (e) => {
+    const target = e.target;
+    if (target.classList.contains("grid-cell") && !target.classList.contains("filled")) {
+      target.classList.add("filled");
+    }
+  });
 
-  for (let col = 0; col < GRID_COLS; col++) {
-    const gridCell = document.createElement("div");
-    gridCell.classList.add("grid-cell");
-    gridRow.appendChild(gridCell);
-  }
+  newGridBtn.addEventListener("click", () => {
+    let gridSize;
 
-  gridContainer.appendChild(gridRow);
+    do {
+      gridSize = +prompt("How many squares per side would you like? (eg. 64)", 64);
+
+    } while (gridSize < 2 || gridSize > 100 || typeof gridSize !== "number");
+
+    clearGrid();
+    createGrid(gridSize, gridSize);
+  });
+
+  createGrid(GRID_SIZE, GRID_SIZE);
 }
 
-gridContainer.addEventListener("mouseover", (e) => {
-  const target = e.target;
-  if (target.classList.contains("grid-cell") && !target.classList.contains("filled")) {
-    target.classList.add("filled");
+function clearGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
   }
-});
+}
+
+function createGrid(rows, cols) {
+  for (let row = 0; row < rows; row++) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+
+    for (let col = 0; col < cols; col++) {
+      const gridCell = document.createElement("div");
+      gridCell.classList.add("grid-cell");
+      gridRow.appendChild(gridCell);
+    }
+
+    gridContainer.appendChild(gridRow);
+  }
+}
+
+setupApp();
